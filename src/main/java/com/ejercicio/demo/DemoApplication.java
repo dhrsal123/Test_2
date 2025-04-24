13f67c530e6de2a274cd8f443cf0c34de6801b25
desktop <rstdtas@proton.me>
2025-04-24 16:21:06 +0200

    Remove unused dependencies

--- src/main/java/com/ejercicio/demo/DemoApplication.java
+++ src/main/java/com/ejercicio/demo/DemoApplication.java
@@ -1,18 +1,7 @@
 package com.ejercicio.demo;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.ejercicio.demo.entity.PhoneEntity;
-import com.ejercicio.demo.entity.RegistryEntity;
-import com.ejercicio.demo.repository.RegistryRepository;
 
 @SpringBootApplication
 public class DemoApplication {
